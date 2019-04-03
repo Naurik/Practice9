7f3d4201d6e4279ad24ad999c7c2bf12c8c05f38
Nauryzbai <nauzik@mail.ru>
2019-04-03 18:22:58 +0600

    Practice9

---
+++ Practice9/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice9
+{
+    public struct Employee
+    {
+        public string Name { get; set; }
+        public Vacancies Vacancy { get; set; }
+        public int Salary { get; set; }
+        public int[] DateOfEngagement { get; set; }
+
+        public Employee(string name, Vacancies vacancy, int salary, int[] dateOfEngagement)
+        {
+            Name = name;
+            Vacancy = vacancy;
+            Salary = salary;
+            DateOfEngagement = dateOfEngagement;
+        }
+        public void Show()
+        {
+            Console.Write($"Name: {Name}" +
+                $"\nVacancy: {Vacancy}" +
+                $"\nSalary: {Salary}" +
+                $"\nDateOfEngagement: ");
+            foreach (var item in DateOfEngagement)
+            {
+                Console.Write(item + ".");
+            }
+        }
+
+    }
